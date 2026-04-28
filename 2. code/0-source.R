9d3a0d5134433cb1853b8128fe2115bb1f99e0c8
Nick Brown <52330932+nickawbrown@users.noreply.github.com>
2026-04-28 13:46:24 -0700

    create source file for R scripts

---
+++ 2. code/0-source.R
@@ -0,0 +1,10 @@
+# Common packages and functions --------------------------------------------------
+
+common_pkgs <- c("tidyverse", "here", "readxl", "writexl", "janitor")
+
+
+library(here)
+library(tidyverse); theme_set(theme_bw())
+library(readxl)
+library(writexl)
+library(janitor)
